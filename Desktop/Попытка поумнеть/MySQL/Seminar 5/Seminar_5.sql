b1b323bc45330e7bdeaab04155fe29c923824967
Maksim Malkov <113548800+MaksimMalkovV@users.noreply.github.com>
2023-06-08 14:37:43 +0300

    Add files via upload

---
+++ Desktop/Попытка поумнеть/MySQL/Seminar 5/Seminar_5.sql
@@ -0,0 +1,59 @@
+DROP DATABASE IF EXISTS lesson_5;
+CREATE DATABASE lesson_5;
+USE lesson_5;
+
+CREATE TABLE cars
+(
+	id INT NOT NULL PRIMARY KEY,
+    name VARCHAR(45),
+    cost INT
+);
+
+INSERT cars
+VALUES
+	(1, "Audi", 52642),
+    (2, "Mercedes", 57127 ),
+    (3, "Skoda", 9000 ),
+    (4, "Volvo", 29000),
+	(5, "Bentley", 350000),
+    (6, "Citroen ", 21000 ), 
+    (7, "Hummer", 41400), 
+    (8, "Volkswagen ", 21600);
+    
+SELECT *
+FROM cars;
+
+CREATE VIEW Cars_25 
+AS SELECT * 
+FROM cars
+WHERE cost < 25000;
+
+SELECT * FROM Cars_25;
+
+ALTER VIEW Cars_25
+AS SELECT * 
+FROM cars
+WHERE cost < 30000;
+
+CREATE VIEW skoda_audi
+AS SELECT *
+FROM cars
+WHERE name = "Audi" OR name = "Skoda";
+
+SELECT * FROM skoda_audi;
+
+CREATE TABLE stations
+(
+train_id INT NOT NULL,
+station varchar(20) NOT NULL,
+station_time TIME NOT NULL
+);
+INSERT stations(train_id, station, station_time)
+VALUES (110, "SanFrancisco", "10:00:00"),
+(110, "Redwood Sity", "10:54:00"),
+(110, "Palo Alto", "11:02:00"),
+(110, "San Jose", "12:35:00"),
+(120, "SanFrancisco", "11:00:00"),
+(120, "Palo Alto", "12:49:00"),
+(120, "San Jose", "13:30:00");
+
